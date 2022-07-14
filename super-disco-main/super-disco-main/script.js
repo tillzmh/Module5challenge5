4646b6b95df7875df490a33dd989751be0bf7f4c
tillzmh <tillzmh@users.noreply.github.com>
2022-07-13 21:35:17 -0400

    added else, if function to change color for row

--- super-disco-main/super-disco-main/script.js
+++ super-disco-main/super-disco-main/script.js
@@ -5,5 +5,21 @@
     const row=$(".time-block");
     const data = getData();
 
+    let currenthours=false;
+    $.each(row, function(i, row){
+        if(row.id==="Hour" + currenthour){
+            $(row).find("textarea").addClass("present");
+            currenthours=true;
+        }
+        else{
+            if(currenthours){
+                $(row).find("textarea").addClass("future");
+            }
+            else{
+                $(row).find("textarea").addClass("past");
+            }
+        }
+    });
+
 
 
