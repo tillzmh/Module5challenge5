374e4b3f04be73df03bcd81030c594bea053e159
tillzmh <tillzmh@users.noreply.github.com>
2022-07-14 14:24:42 -0400

    button should now be triggers when text is entered

--- super-disco-main/super-disco-main/script.js
+++ super-disco-main/super-disco-main/script.js
@@ -5,20 +5,27 @@
     const row=$(".time-block");
     const data = getData();
 
-    let currenthours=false;
+    let getcurrenthour=false;
     $.each(row, function(i, row){
         if(row.id==="Hour" + currenthour){
             $(row).find("textarea").addClass("present");
-            currenthours=true;
+            getcurrenthours=true;
         }
         else{
-            if(currenthours){
+            if(getcurrenthours){
                 $(row).find("textarea").addClass("future");
             }
             else{
                 $(row).find("textarea").addClass("past");
             }
         }
+        if(data[row.id]){
+        $(row).find("textarea").val(data[row.id]);
+    }
+    $(row).find("button").on("click",function(){
+        data[row.id]=$(row).find("textarea").val();
+        setdata(data);
+    });
     });
 
 
