d2554268551d2a019deaa28c5d0afe2001f28e44
tillzmh <tillzmh@users.noreply.github.com>
2022-07-13 20:28:29 -0400

    readded id, new approach for jquerry

--- super-disco-main/super-disco-main/script.js
+++ super-disco-main/super-disco-main/script.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
-    $(".saveBtn").click(function(){
-        var time = $(this).parent().attr("id");
-        var text = $(this).siblings(".description").val();
-
-        localStorage.setItem(time, text);
-        localStorage.getItem(time, text);
-        
-    })
-
-
-
-
-
-
-
 
 }) 
 
